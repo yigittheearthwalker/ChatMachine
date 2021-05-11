fbb6af11c218333e58fcd0cde2f2e1583fcf5811
Eyüp Yiğit Dilber <eyupyigitdilber@gmail.com>
2021-05-11 18:26:24 +0300

    procfile

--- src/main/java/com/mychatmachine/ChatMachine/DriverChatMachine.java
+++ src/main/java/com/mychatmachine/ChatMachine/DriverChatMachine.java
@@ -4,7 +4,7 @@ import com.mychatmachine.server.ChatServer;
 
 public class DriverChatMachine {
 	public static void main(String[] args) {
-		ChatServer chatServer = new ChatServer();
+		ChatServer chatServer = new ChatServer(80);
 		chatServer.start();	
 		
 	}
